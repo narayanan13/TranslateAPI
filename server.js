ab4239ba8ab3ddba22dcdf5b4617e906a2dff0f9
narayanan13 <narayananofficial13@gmail.com>
2025-02-13 18:14:26 +0530

    update-2

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import express, { json } from "express";
-import { get } from "axios";
+import express from "express";
+import axios from "axios";
 import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 1417;
 
 app.use(cors());
-app.use(json()); // Middleware to parse JSON requests
+app.use(express.json()); // Middleware to parse JSON requests
 
 // Translation API Endpoint
 app.get("/translate", async (req, res) => {
@@ -22,7 +22,7 @@ app.get("/translate", async (req, res) => {
         const url = `https://lingva.ml/api/v1/${source}/${target}/${encodeURIComponent(text)}`;
 
         // Make the API request
-        const response = await get(url);
+        const response = await axios.get(url);
 
         // Send back the API response
         res.json(response.data);
